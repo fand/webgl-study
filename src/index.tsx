1543bb5e330ba926ef6deb3f421a5889bcaf5c42
fand <fand.gmork@gmail.com>
2017-05-13 02:23:19 +0900

    day 18

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './app';
 declare function require(name: string);
 
 const shaders = [
+    require('./shaders/18.frag'),
     require('./shaders/17.frag'),
     require('./shaders/16.frag'),
     require('./shaders/15.frag'),
@@ -25,6 +26,7 @@ const shaders = [
     // require('./shaders/0.frag'),
 ];
 const texts = [
+    require('./shaders/18.md'),
     require('./shaders/17.md'),
     require('./shaders/16.md'),
     require('./shaders/15.md'),
